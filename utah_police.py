ceeb8d7c89e2e099498a71e3ab903771126c9ec7
wtbarras <williamtbarras@gmail.com>
2018-08-13 19:57:29 -0600

    Remove location parsing. The data uses a grid system that I can't map to longitude and latitude

--- utah_police.py
+++ utah_police.py
@@ -30,27 +30,3 @@ types = police_data.groupby('cr_desc').count()
 types = types.reset_index()
 print('Grouped Data')
 print(types.head())
-
-
-# Data has grid coordinates, not (lat,lon)
-# -- Form map --
-# map = Basemap(projection='merc', lat_0 = 57, lon_0 = -135,
-#     resolution = 'h', area_thresh = 0.1,
-#     llcrnrlon=-136.25, llcrnrlat=56.0,
-#     urcrnrlon=-134.25, urcrnrlat=57.75)
-
-# Salt Lake coordinates   : 40.766615, -111.939750
-# Upper right coordinates : 40.893015, -111.698564
-# Lower left coordinates  : 40.668106, -112.234947
-# map = Basemap(projection='merc', lat_0 = 57, lon_0 = -135,
-#     resolution = 'h', area_thresh = 0.1,
-#     llcrnrlon=-112.23, llcrnrlat=40.67,
-#     urcrnrlon=-111.7, urcrnrlat=40.89)
- 
-# map.drawcoastlines()
-# map.drawcountries()
-# map.drawrivers(linewidth=0.5, linestyle='solid', color='k', antialiased=1, ax=None, zorder=None)
-# map.fillcontinents(color = 'coral')
-# map.drawmapboundary()
- 
-# plt.show()
