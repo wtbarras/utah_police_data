e683ed17497d7d29f7ca769b27fe723568f23d1a
wtbarras <williamtbarras@gmail.com>
2018-08-14 20:58:37 -0600

    Remove unneeded code and clean up output

--- utah_police.py
+++ utah_police.py
@@ -14,43 +14,40 @@ with open('police.json') as f:
 # Set up columns
 print('creating columns')
 columns = [dct['id'] for dct in police_data_json['fields']]
-print('columns:')
-print(columns)
+# print('columns:')
+# print(columns)
 
 # Create dataframe
 print('converting to dataframe. . .')
 police_data = pd.DataFrame(police_data_json['records'], columns=columns)
 
-print('first five rows of data:')
-print(police_data.head())
+# print('first five rows of data:')
+# print(police_data.head())
 
+# Clean data
+print('cleaning data. . .')
 # Strip occ_date field down to just date
 police_data['occ_date'] = police_data.occ_date.str[:10]
 # print(police_data['occ_date'].head())
-
 # Clean cr_desc values
 police_data['cr_desc'] = police_data.cr_desc.str.strip()
 
-# Group by crime type
-types = police_data.groupby('cr_desc').count()
-types = types.reset_index()
-# print('Grouped Data')
-# print(types)
-
 # Find LARCENY data by date
-# df = df[df['groupby_column'].isin(ids)]
+print('processing larceny data')
 type_and_date = police_data.groupby(['cr_desc', 'occ_date']).count()
+# print("Grouped by type and date:")
 # print(type_and_date)
 larceny_data = type_and_date.loc['LARCENY'].reset_index()
-print(larceny_data)
+# print(larceny_data)
 
 # Format dates
+print('formatting dates. . .')
 larceny_data['formatted_date'] = [dt.datetime.strptime(d,'%Y-%m-%d').date() for d in larceny_data.occ_date]
 
 # Plot data
+print('plotting data. . .')
 fig, ax = plt.subplots()
 ax.plot(larceny_data['formatted_date'], larceny_data['_id'])
 # Rotate date labels automatically
 fig.autofmt_xdate()
-
 plt.show()
